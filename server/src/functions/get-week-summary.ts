3d6bd979ec5706a06189317be407002c0aad5bbb
Isaac Prado <isaac.prado@requestia.com>
2024-09-13 16:39:57 -0300

    Cria e faz a tipagem da propriedade `goalsPerDay`

--- server/src/functions/get-week-summary.ts
+++ server/src/functions/get-week-summary.ts
@@ -57,6 +57,15 @@ export async function getWeekSummary() {
       .groupBy(goalsCompletedInWeek.completedAtDate)
   );
 
+  type GoalsPerDay = Record<
+    string,
+    {
+      id: string;
+      title: string;
+      completedAt: string;
+    }[]
+  >;
+
   const result = await db
     .with(goalsCreatedUpToWeek, goalsCompletedInWeek, goalsCompletedByWeekDay)
     .select({
@@ -68,7 +77,7 @@ export async function getWeekSummary() {
         sql/*SQL*/ `(SELECT SUM(${goalsCreatedUpToWeek.desiredWeeklyFrequency}) FROM ${goalsCreatedUpToWeek})`.mapWith(
           Number
         ),
-      goalsPerDay: sql/*SQL */ `
+      goalsPerDay: sql/*SQL */ <GoalsPerDay>`
 				JSON_OBJECT_AGG(/**CRIA UM OBJETO */
 					${goalsCompletedByWeekDay.completedAtDate},
 					${goalsCompletedByWeekDay.completions}
