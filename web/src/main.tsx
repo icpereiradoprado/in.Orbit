d226d92ba94feffb26a1c3aac752a26fc0c0de19
Isaac Prado <isaac.prado@requestia.com>
2024-09-13 17:01:01 -0300

    [Parcial] Configura o consumo da API no componente `App` utilizando o `TanStack Query` | Endpoint: `summary`

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import "./index.css";
 
+const queryClient = new QueryClient();
 const rootElement = document.getElementById("root");
 if (rootElement) {
 	createRoot(rootElement).render(
 		<StrictMode>
-			<App />
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
 		</StrictMode>,
 	);
 }
